2d58f2a7e162f1eb1e5eebfff60b5683b6a82306
NicolasRamirez23 <nira_21@alu.uabcs.mx>
2023-06-26 15:33:22 -0700

    Conexion de archivo HTML y CSS

--- login/index.php
+++ login/index.php
@@ -4,32 +4,35 @@
 
   <title>Log in</title>
 
+  <link rel="stylesheet" href="../css/style.css">
+
 </head>
 
 <body>
+  <div id="login1">
+    <h1 id="bienvenida">Ingresa los datos que se te pidan</h1>
 
-  <h1>Ingresa los datos que se te pidan</h1>
-
-  <form action="index.php" method="POST">
-    <!---Label Usuario e Input-->
-    <label for="usuario">Usuario</label>
-    <br>
-    <input type="text" id="usuario" name="usuario" placeholder="ej. pepe123" 
-    required>
+    <form action="index.php" method="POST">
+      <!---Label Usuario e Input-->
+      <label for="usuario">Usuario</label>
+      <br>
+      <input type="text" id="usuario" name="usuario" placeholder="ej. pepe123" 
+      required>
+      
+      <br>
+      <br>
+      
+      <!--Label Clave e Input-->
+      <label for="clave">Clave</label>
+      <br>
+      <input type="password" id="clave" name="clave" placeholder="Contraseña"
+      required>
     
-    <br>
-    <br>
-    
-    <!--Label Clave e Input-->
-    <label for="clave">Contraseña</label>
-    <br>
-    <input type="password" id="clave" name="clave" placeholder="Contraseña"
-    required>
-   
-    <br>
-    <br>
+      <br>
+      <br>
 
-    <button type="submit" name="submit">Ingresar</button>
+      <button type="submit" name="submit">Ingresar</button>
+  </div>
 </form>
   
 <?php
@@ -40,36 +43,13 @@
     $usuario = $_POST['usuario'];
     $clave = $_POST['clave'];
 
-    // Conexión a la base de datos
-    $servername = "localhost";
-    $username_db = "root";
-    $password_db = "";
-    $dbname = "formulario";
-
-    // Crear la conexión
-    $conn = new mysqli($servername, $username_db, $password_db, $dbname);
-
-    // Verificar la conexión (preguntar)
-    if ($conn->connect_error) {
-      die("Error de conexión a la base de datos: " . $conn->connect_error);
+    if($usuario == 'admin' && $clave == '1234'){
+      echo("datos correctos");
+      header("Location: http://localhost/DesarrolloWeb/dashboard/index.php");
+      
+    }else{
+      echo("datos incorrectos");
     }
-
-    // Consulta para verificar los datos de inicio de sesión
-    $sql = "SELECT * FROM admin WHERE usuario='$usuario' AND clave='$clave'";
-    $result = $conn->query($sql);
-
-    // Verificar si se encontró un registro coincidente
-    if ($result->num_rows == 1) {
-
-      header("Location: http://localhost/ssat/dashboard/index.php");
-      exit();
-           
-    } else {
-        
-      echo "<p>Error: Usuario o contraseña incorrectos.</p>";
-    }
-
-    $conn->close();
   }
   ?>
   
